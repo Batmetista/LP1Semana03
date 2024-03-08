cdd4e297bf65539cf9c8e63726fa70a82d421eda
Batmetista <batista.as.pedro@gmail.com>
2024-03-08 16:26:20 +0000

    EX2

---
+++ ChangeString/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ask the user for a string
+            Console.Write("String: ");
+            string s = Console.ReadLine();
+
+
+            //ask the user for a character
+            Console.Write("Caráter: ");
+            string character = Console.ReadLine();
+            char ch = char.Parse(character);
+
+            string new_s = "";
+
+            foreach (char c in s)
+            { 
+                if(c == ch)
+                {
+                    new_s += "X";
+                }
+                    
+                else
+                {
+                    new_s += c;
+                }
+            }
+            Console.WriteLine(new_s);
+
+        }
+    }
+}

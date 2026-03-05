1d59c9572e545cd6e42d88b7d36a481ef15b8d6b
Pedro Batista <batista.as.pedro@gmail.com>
2026-03-05 16:57:56 +0000

    CyberHack

---
+++ ArrayMul/Program.cs
@@ -0,0 +1,22 @@
+﻿
+public class Program
+{
+    /// <summary>
+    /// Método principal
+    /// </summary>
+    /// <param name="args"></param>
+    private static void Main(string[] args)
+    {
+        
+        foreach (float i in args)
+        {
+            float [][] A = new float [1] [1] {i};
+            float [][] A = new float [1] [2] {i++};
+            float [][] A = new float [2] [1] {i++};
+            float [][] A = new float [2] [2] {i++};
+            float [][] b = new float [1] [1] {i++};
+            float [][] b = new float [2] [1] {i++};
+            Console.WriteLine(A);
+        }
+    }
+}
---
+++ CyberHack/Program.cs
@@ -0,0 +1,99 @@
+﻿public enum HackTool
+{
+    BruteForce,
+    Phishing,
+    Backdoor,
+    ZeroDay,
+    AIOverride,
+    QuantumExpoit
+}
+public enum SystemType
+{
+    CorporateServer,
+    BankDatabase,
+    SmartCityCore,
+    MilitaryAI
+}
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        SystemType System;
+        HackTool Tool1;
+        HackTool Tool2;
+
+        string S =Console.ReadLine();
+
+        if (S == "CorporateServer")
+        {
+            System = SystemType.CorporateServer;
+        }
+        if (S == "BankDataBase")
+        {
+            System = SystemType.BankDatabase;
+        }
+        if (S == "SmartCityCore")
+        {
+            System = SystemType.SmartCityCore;
+        }
+        if (S == "MilitaryAI")
+        {
+            System = SystemType.MilitaryAI;
+        }
+        
+        string t1 = Console.ReadLine();
+
+        if (t1 == "BruteForce")
+        {
+            Tool1 = HackTool.BruteForce;
+        }
+        if (t1 == "Phishing")
+        {
+            Tool1 = HackTool.Phishing;
+        }
+        if (t1 == "Backdoor")
+        {
+            Tool1 = HackTool.Backdoor;
+        }
+        if (t1 == "ZeroDay")
+        {
+            Tool1 = HackTool.ZeroDay;
+        }
+        if (t1 == "AIOverride")
+        {
+            Tool1 = HackTool.AIOverride;
+        }
+        if (t1 == "QuantumExpoit")
+        {
+            Tool1 = HackTool.QuantumExpoit;
+        }
+
+        string t2 = Console.ReadLine();
+
+        if (t2 == "BruteForce")
+        {
+            Tool2 = HackTool.BruteForce;
+        }
+        if (t2 == "Phishing")
+        {
+            Tool2 = HackTool.Phishing;
+        }
+        if (t2 == "Backdoor")
+        {
+            Tool2 = HackTool.Backdoor;
+        }
+        if (t2 == "ZeroDay")
+        {
+            Tool2 = HackTool.ZeroDay;
+        }
+        if (t2 == "AIOverride")
+        {
+            Tool2 = HackTool.AIOverride;
+        }
+        if (t2 == "QuantumExpoit")
+        {
+            Tool2 = HackTool.QuantumExpoit;
+        }
+        
+    }
+}
